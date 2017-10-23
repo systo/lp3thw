38810f9b704a30643cec20eedf814efa0fbc0b4b
systo <15274015+systo@users.noreply.github.com>
2017-10-23 03:29:04 -0500

    Adding previous exercises and study drills: ex01/drills, ex02, ex03/drills, and ex04

---
+++ lp3thw/ex03_sds.py
@@ -0,0 +1,37 @@
+# Prints out a statement
+print("I will now count my chickens:")
+
+#Prints out a string, then a float
+print("Hens", 25 + 30 / 6)
+#Prints out a string, then an integer
+print("Roosters", 100 - 25 * 3 % 4)
+
+#prints out another string
+print("Now i will count the eggs:")
+
+#prints the result of a math equation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#prints a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#prints a math equation
+print(3 + 2 < 5 - 7)
+
+#prints a string then an integer
+print("What is 3 + 2?", 3 + 2)
+#prints a string then an integer 
+print("What is 5 - 7?", 5 - 7)
+
+# prints another string
+print ("Oh, that's why it's False.")
+
+#prints another string
+print("How about some more.")
+
+#prints a string, then a boolean
+print("Is it greater?", 5 > -2)
+#prints a string, then another boolean
+print("Is it greater or equal?", 5 >= -2)
+#prints a string then the final boolean
+print("Is it less or equal?", 5 <= -2)
