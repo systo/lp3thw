c8919e645ebea0473d3b30e27f48afd95b805149
systo <15274015+systo@users.noreply.github.com>
2017-11-13 15:14:11 -0600

    Adding ex07 study drill 1

---
+++ lp3thw/ex07_sd1.py
@@ -0,0 +1,26 @@
+# prints a line
+print("Mary had a little lamb.")
+# prints a line
+print("Its fleece was white as {}.".format('snow'))
+# prints a line
+print("And everywhere that Mary went.")
+# pirnts a period 10 times
+print("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' and the end. try removing it to see what happens
+# end changes the end of the string to a space instead of a new line
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
